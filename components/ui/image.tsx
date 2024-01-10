d85ea5a093e9eae84c4b22bfca80610ca6995849
Medinaa <71106038+MedinaYT@users.noreply.github.com>
2024-01-10 19:24:12 +0100

    update gitignore

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -1,5 +1,5 @@
 import cn from "@/lib/cn";
-import NextImage from "next/image";
+// import NextImage from "next/image";
 
 type Props = {
     src: string;
@@ -11,10 +11,10 @@ type Props = {
 const Image = ({ src, alt, isBlurred, className }: Props) => {
     return (
         <div className="relative z-10 w-full h-full">
-            <NextImage src={src} alt={alt} className={className} />
+            <img src={src} alt={alt} className={className} />
             {isBlurred && (
                 <div className="absolute -z-10 inset-0 w-full h-full blur-xl">
-                    <NextImage
+                    <img
                         src={src}
                         alt={alt}
                         className={cn(className, "opacity-75")}
