37123b955110e1c8326fcda4ad2e29fee2e23f74
Medinaa <themario92017@gmail.com>
2024-01-12 16:07:33 +0100

    solved errors of link query

--- app/api/link/[alias]/route.ts
+++ app/api/link/[alias]/route.ts
@@ -1,4 +1,4 @@
-import prisma from "@/lib/db"
+import { prisma } from "@/lib/db"
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { alias: string } }) {
