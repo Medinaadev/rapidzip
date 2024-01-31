7256bebe8df030fa25e4704e6d770f11177de5a1
Medinaa <themario92017@gmail.com>
2024-01-31 17:04:02 +0100

    fixed meteors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,12 +17,12 @@ const config: Config = {
                 meteor: {
                     "0%": {
                         transform: "rotate(215deg) translateX(0)",
-                        opacity: 1,
+                        opacity: "1",
                     },
-                    "70%": { opacity: 1 },
+                    "70%": { opacity: "1" },
                     "100%": {
                         transform: "rotate(215deg) translateX(-1000px)",
-                        opacity: 0,
+                        opacity: "0",
                     },
                 },
             },
