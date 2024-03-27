f74e0ba5208218d7be001292f885ab4439e20060
Medinaadev <themario92017@gmail.com>
2024-03-27 02:45:00 +0100

    add vercel analytics

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -6,36 +6,42 @@ import TrpcProvider from "@/app/_trpc/TrpcProvider";
 import { EditLinkModalProvider } from "@/components/modals/editLink";
 import { CreatedLinkModalProvider } from "@/components/modals/createdLink";
 import { DeleteLinkModalProvider } from "@/components/modals/deleteLink";
+import { Analytics } from "@vercel/analytics/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-        <SessionProvider>
-            <TrpcProvider>
-                <NextUIProvider>
-                    <CreatedLinkModalProvider>
-                        <EditLinkModalProvider>
-                            <DeleteLinkModalProvider>
-                                <Toaster
-                                    position="top-center"
-                                    toastOptions={{
-                                        style: {
-                                            background: "rgba(25, 33, 44, 0.8)",
-                                            border: "1px solid rgb(255, 255, 255, 0.1)",
-                                            color: "#ccc",
-                                            paddingBlock: ".3rem",
-                                            paddingInline: ".5rem",
-                                            boxShadow:
-                                                "0 0 10px rgba(0, 0, 0, 0.2)",
-                                        },
-                                    }}
-                                />
-                                {children}
-                            </DeleteLinkModalProvider>
-                        </EditLinkModalProvider>
-                    </CreatedLinkModalProvider>
-                </NextUIProvider>
-            </TrpcProvider>
-        </SessionProvider>
+        <>
+            <Analytics />
+
+            <SessionProvider>
+                <TrpcProvider>
+                    <NextUIProvider>
+                        <CreatedLinkModalProvider>
+                            <EditLinkModalProvider>
+                                <DeleteLinkModalProvider>
+                                    <Toaster
+                                        position="top-center"
+                                        toastOptions={{
+                                            style: {
+                                                background:
+                                                    "rgba(25, 33, 44, 0.8)",
+                                                border: "1px solid rgb(255, 255, 255, 0.1)",
+                                                color: "#ccc",
+                                                paddingBlock: ".3rem",
+                                                paddingInline: ".5rem",
+                                                boxShadow:
+                                                    "0 0 10px rgba(0, 0, 0, 0.2)",
+                                            },
+                                        }}
+                                    />
+                                    {children}
+                                </DeleteLinkModalProvider>
+                            </EditLinkModalProvider>
+                        </CreatedLinkModalProvider>
+                    </NextUIProvider>
+                </TrpcProvider>
+            </SessionProvider>
+        </>
     );
 };
 
